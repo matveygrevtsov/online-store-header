0953f9c550c048c53fddb0ad2162ba3fd03363a9
matveygrevtsov <43029770+matveygrevtsov@users.noreply.github.com>
2024-08-10 13:41:46 +0300

    add-module-federation-plugin (#3)
    Co-authored-by: matveygrevtsov <matveygrevcov@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,40 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { ModuleFederationPlugin } = require("webpack").container;
+const deps = require("./package.json").dependencies;
 
 const OUTPUT_FOLDER_NAME = path.resolve(__dirname, "dist"); // Папка, куда всё заливаться сбилженный проект.
+
+const moduleFederationConfig = {
+  name: "header",
+  filename: "remoteEntry.js",
+  exposes: {
+    "./Header": "./src/components/Header/Header",
+  },
+  shared: {
+    ...deps,
+    react: {
+      singleton: true,
+      requiredVersion: deps.react,
+      eager: true,
+    },
+    "react-dom": {
+      singleton: true,
+      requiredVersion: deps["react-dom"],
+      eager: true,
+    },
+    "react-router-dom": {
+      singleton: true,
+      requiredVersion: deps["react-router-dom"],
+      eager: true,
+    },
+    antd: {
+      singleton: true,
+      requiredVersion: deps.antd,
+      eager: true,
+    },
+  },
+};
 
 module.exports = {
   mode: "development",
@@ -39,6 +72,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: "./public/index.html",
     }),
+    new ModuleFederationPlugin(moduleFederationConfig),
   ],
   devServer: {
     static: {
